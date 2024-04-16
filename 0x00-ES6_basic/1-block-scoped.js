5e4c58f6761746959e0baaaa6fa1f3694c0df767
roxxe34 <hamzafarissi7@gmail.com>
2024-04-16 14:41:36 +0000

    done

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+export default function taskBlock(trueOrFalse) {
+  let task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    var task = true;
+    var task2 = false;
+  }
+
+  return [task, task2];
+}
+
