476c7feb85b630312b99f86f95eba4970b19f112
Hamza Farissi <hamzafarissi7@gmail.com>
2024-06-25 23:08:42 +0100

    Refactor handleProfileSignup function to use ES6 import syntax and process photo data

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,1 +1,11 @@
-#!/usr/bin/node
+import { uploadPhoto, createUser } from './utils';
+
+function processPhotoData([photoData, userData]) {
+  console.log(`${photoData.body} ${userData.firstName} ${userData.lastName}`);
+}
+
+export default function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()]).then((values) => {
+    processPhotoData(values);
+  });
+}
