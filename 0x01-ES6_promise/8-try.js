0f333b8c6fa2ae88adeb96313b1f8074aeb33f99
roxxe34 <hamzafarissi7@gmail.com>
2024-06-26 19:17:49 +0100

    chore: Refactor divideFunction to handle division by zero using Number.isFinite

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,6 +1,6 @@
 export default function divideFunction(numerator, denominator) {
 const res = numerator / denominator
-if (denominator === 0) {
+if (Number.isFinite(res)) {
 throw new Error("cannot divide by 0");
 }
 else {
