3111e2e12cddbb0ef8682f39d25861cebfbde041
Hamza Farissi <hamzafarissi7@gmail.com>
2024-06-26 02:11:11 +0100

    Refactor handleProfileSignup function to use more descriptive variable names

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,12 @@
+/* eslint-disable import/extensions */
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const photo = uploadPhoto();
-  const user = createUser();
-  Promise.all([photo, user]).then((values) => {
-    console.log(`${values[0].body} ${values[1].firstName} ${values[0].lastName}`);
+  const upload = uploadPhoto();
+  const create = createUser();
+  return Promise.all([upload, create]).then((result) => {
+    console.log(`${result[0].body} ${result[1].firstName} ${result[1].lastName}`);
   }).catch(() => {
-    console.error('Signup system offline');
+    console.log('Signup system offline');
   });
 }
