a0fd6f2e8f3f4514a3ce890c77ad297ca90a8435
roxxe34 <hamzafarissi7@gmail.com>
2024-06-26 19:20:16 +0100

    done

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,9 +1,7 @@
 export default function divideFunction(numerator, denominator) {
-const res = numerator / denominator
-if (Number.isFinite(res)) {
-throw new Error("cannot divide by 0");
-}
-else {
-  return res
-}
+  if (denominator === 0) {
+    throw Error('cannot divide by 0');
+  } else {
+    return numerator / denominator;
+  }
 }
