a3c4c81c270f996c9c6e0be9a9f4819287adf275
Hamza Farissi <hamzafarissi7@gmail.com>
2024-06-26 13:03:44 +0100

    Refactor handleProfileSignup function to use Promise.allSettled

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,17 +1,13 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const res = [];
-  try {
-    const user = await signUpUser(firstName, lastName);
-    res.push({ status: 'fulfilled', value: user });
-    await uploadPhoto(fileName);
-  } catch (err) {
-    res.push({
-      status: 'rejected',
-      value: `Error: ${fileName} cannot be processed`,
-    });
-  }
-  return res;
+export default function handleProfileSignup(firstName, lastName, fileName) {
+	return Promise
+		.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+		.then((results) => (
+			results.map((result) => ({
+				status: result.status,
+				value: result.status === 'fulfilled' ? result.value : String(result.reason),
+			}))
+		));
 }
