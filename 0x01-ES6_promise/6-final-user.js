573fc5e3e63552397483509b463ca8e1cbd3b4f0
Hamza Farissi <hamzafarissi7@gmail.com>
2024-06-26 12:43:28 +0100

    Remove unnecessary console.log statement in handleProfileSignup function

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,7 +3,5 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => {
-      console.log(results);
-    });
+    ;
 }
