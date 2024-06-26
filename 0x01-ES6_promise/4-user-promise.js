cdbbbab8bae03795fea2ec875e0944f243102805
Hamza Farissi <hamzafarissi7@gmail.com>
2024-06-26 02:29:24 +0100

    Add signUpUser function to create a user promise

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,1 +1,10 @@
-#!/usr/bin/node
+export default function signUpUser(firstName, lastName) {
+  return new Promise((resolve) => {
+    resolve(
+      {
+        firstName,
+        lastName,
+      },
+    );
+  });
+}
