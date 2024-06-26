6e229d5e96c962032db7dc427179ddb6fd76e911
roxxe34 <hamzafarissi7@gmail.com>
2024-06-26 18:26:05 +0100

    Refactor loadBalancer function to use Promise.race for faster download selection

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,1 +1,5 @@
-#!/usr/bin/node
+export default function loadBalancer(chinaDownload, USDownload) {
+return Promise.race([chinaDownload, USDownload]).then((results) => {
+  console.log(results);
+})
+}
