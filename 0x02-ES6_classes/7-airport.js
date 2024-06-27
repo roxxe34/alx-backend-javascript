9b2f459b498115b871bc492377e4a0f311d17ed5
Hamza Farissi <hamzafarissi7@gmail.com>
2024-06-27 21:25:31 +0100

    Refactor Airport class to include name and code properties

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,1 +1,10 @@
-#!/usr/bin/node
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  get [Symbol.toStringTag]() {
+    return this._code;
+  }
+}
