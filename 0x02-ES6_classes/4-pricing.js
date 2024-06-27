c2e3b64bcf965630b97aacb4c9b1b05bd236aa87
roxxe34 <hamzafarissi7@gmail.com>
2024-06-27 13:46:03 +0100

    Refactor Pricing class to use ES6 syntax and add getters and setters

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,6 +1,6 @@
 import Currency from "./3-currency.js";
 
-export default class {
+export default class Pricing{
   constructor(amount, currency) {
     this.amount = amount;
     this.currency = currency;
