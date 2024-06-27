29b673b53f0f25d593116fbda1de71e284ed4690
roxxe34 <hamzafarissi7@gmail.com>
2024-06-27 12:56:35 +0100

    Refactor Currency class to use ES6 syntax and add getters and setters

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,1 +1,33 @@
-#!/usr/bin/node
+export default class Currency {
+  constructor(code, name){
+    this.code = code
+    this.name = name
+  }
+
+  get code() {
+    return this._code
+  }
+
+  set code(val) {
+    if (typeof val != "string") {
+      throw new Error("Code must be a string")
+    } else {
+      this._code = val
+    }
+  }
+
+  get name() {
+    return this._name
+  }
+
+  set name(val) {
+    if (typeof val != "string") {
+      throw new Error("Name must be a string")
+    } else {
+      this._name = val
+    }
+  }
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`
+  }
+}
