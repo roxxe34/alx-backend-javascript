444e6db73880d5b234a25fa599a0d39bf5cb659e
roxxe34 <hamzafarissi7@gmail.com>
2024-06-27 13:41:09 +0100

    Refactor Pricing class to use ES6 syntax and add getters and setters

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,1 +1,42 @@
-#!/usr/bin/node
+import Currency from "./3-currency.js";
+
+export default class {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+  get amount() {
+    return this._amount
+  }
+
+  set amount(val) {
+    if (typeof val != "number") {
+      throw new Error("Amount must be a number")
+    } else {
+      this._amount = val
+    }
+  }
+
+  get currency() {
+    return this._currency
+  }
+
+  set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw new TypeError('currency must be a Currency');
+    }
+      this._currency = value
+    }
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name} ${this.currency.code}`
+  }
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('amount must be a number');
+    }
+    if (typeof conversionRate !== 'number') {
+      throw new TypeError('conversionRate must be a number');
+    }
+    return amount * conversionRate;
+  }
+}
