3ac139cef3789cbd6c14ce7e3da85b3a4ac5a083
Hamza Farissi <hamzafarissi7@gmail.com>
2024-06-30 21:08:48 +0100

    feat: Add ES6 data manipulation functions

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,6 @@
+export default function getStudentsByLocation (getListStudents, city) {
+  if (!Array.isArray(getListStudents)) {
+    return [];
+  }
+  return getListStudents.filter(student => student.location == city)
+}
