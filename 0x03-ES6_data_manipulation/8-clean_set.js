ffcc858cbca31f7bc81e1f1913ec7cf4fe58618e
roxxe34 <hamzafarissi7@gmail.com>
2024-07-01 18:45:08 +0100

    Refactor cleanSet function to use strict equality operator and improve code readability

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,8 @@
-export default function cleanSet(set,startString) {
-  if (startString.length == 0 || set.size == 0) {
-    return ''
+export default function cleanSet(set, startString) {
+  if (startString.length === 0 || set.size === 0) {
+    return '';
   }
-  const result = [...set].filter(x => x.startsWith(startString));
-  let sliced = result.map(x => x.slice(startString.length));
-  return sliced.join("-")
+  const result = [...set].filter((x) => x.startsWith(startString));
+  const sliced = result.map((x) => x.slice(startString.length));
+  return sliced.join('-');
 }
