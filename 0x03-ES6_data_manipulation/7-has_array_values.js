32fb3e6bcef07fbca06ed1bf1cb26ca75906fc72
roxxe34 <hamzafarissi7@gmail.com>
2024-07-01 18:34:57 +0100

    feat: Refactor hasValuesFromArray function to use arrow function and return a new Set

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,11 +1,3 @@
 export default function hasValuesFromArray(set, array) {
-  if (array.length == 1)
-    {
-      return set.has(array[0])
-    }
-  const check = array.map(x => set.has(x))
-  const element = false
-  if(check.includes(element)) {
-    return false
-  }
+  return array.every((x) => set.has(x));
 }
