5575ac179f835df44ce1304fd98cd5f633345a85
roxxe34 <hamzafarissi7@gmail.com>
2024-08-20 18:24:47 +0100

    feat: Add interactive input functionality with readline module

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+const readline = require('node:readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
+  console.log(`Your name is: ${name}!`);
+  console.log('This important software is now closing');
+  rl.close();
+});
